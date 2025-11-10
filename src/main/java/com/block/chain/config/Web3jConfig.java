f730c74c778a98ba167401834ac7dd0f6ab6b85f
junseokhub <github.yam117@passinbox.com>
2025-11-10 09:48:40 +0900

    Feat: Web3jConfig

--- src/main/java/com/block/chain/config/Web3jConfig.java
+++ src/main/java/com/block/chain/config/Web3jConfig.java
@@ -1,15 +1,27 @@
 package com.block.chain.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
 import org.web3j.protocol.http.HttpService;
+
+import java.io.IOException;
 
 @Configuration
 public class Web3jConfig {
+    @Value("${spring.blockchain.rpc.url}")
+    private String rpcUrl;
+
+    @Value("spring.blockchain.private.key")
+    private String privateKey;
 
     @Bean
-    public Web3j web3j() {
-        return Web3j.build(new HttpService("http://localhost:8080"));
+    public Web3j web3j() throws IOException {
+        Web3j web3j = Web3j.build(new HttpService(rpcUrl));
+        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+        System.out.println("web3j Web3ClientVersion: " + web3ClientVersion);
+        return web3j;
     }
 }
